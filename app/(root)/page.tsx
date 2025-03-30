46d4ca7196fa52c223bfa2f31219108fb7379613
daynaaaa <danya.eggy@gmail.com>
2025-03-29 22:25:21 -0400

    header & footer

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: `home`,
+};
+
+const HomePage = () => {
+  return ( 
+    <>
+      Home Page
+    </>
+   );
+}
+ 
+export default HomePage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "store"
+export const APP_DESC = process.env.NEXT_PUBLIC_APP_DESC || "made in next js 15"
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000/"
