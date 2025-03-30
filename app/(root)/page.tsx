7d1c4bdcfa8311366ca24895f040dffb83f477a4
daynaaaa <danya.eggy@gmail.com>
2025-03-29 23:38:30 -0400

    theme toggler & menu

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: `home`,
 };
-
-const HomePage = () => {
+const delay = (ms:number) => new Promise((resolve) => setTimeout(resolve, ms));
+const HomePage = async () => {
+  await delay(1000);
   return ( 
     <>
       Home Page
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "@/assets/styles/globals.css";
 import { APP_DESC, APP_NAME, SERVER_URL } from "@/lib/constants";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const lato = Lato({
   weight: ['300', '700'],
@@ -20,9 +21,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${lato.className}`}>
-        {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
