ca935f02d7bebb4a8365d9068db398d974356611
daynaaaa <danya.eggy@gmail.com>
2025-03-30 21:20:37 -0400

    add products (sample data for now)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 // import { Button } from "@/components/ui/button";
 import { Metadata } from "next";
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/products/product-list";
 
 export const metadata: Metadata = {
   title: `home`,
@@ -7,9 +9,10 @@ export const metadata: Metadata = {
 const delay = (ms:number) => new Promise((resolve) => setTimeout(resolve, ms));
 const HomePage = async () => {
   await delay(1000);
+  
   return ( 
     <>
-      Home Page
+      <ProductList data={sampleData.products} title='Latest Arrivals' limit={4} />
     </>
    );
 }
